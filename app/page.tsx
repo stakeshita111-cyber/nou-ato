5056bf205fca1f72da44e61a9e56e1408b1df5eb
Sho Takeshita <stakeshita111@gmail.com>
2026-07-11 09:58:46 +0900

    Supabaseのインポートパスを修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 // さきほど作った通信窓口を呼び出す
-import { supabase } from "../lib/supabase";
+import { supabase } from "../src/lib/supabase";
 
 export default function Home() {
   const [quests, setQuests] = useState<any[]>([]);
